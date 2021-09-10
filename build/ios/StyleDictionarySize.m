d4399f4671dab08554900b1a12402f2d6de1050b
Ibe_Vanmeenen <Ibe_Vanmeenen@mohawkind.com>
2021-09-10 13:42:31 +0200

    initial

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Fri, 10 Sep 2021 11:30:40 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
